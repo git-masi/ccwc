ec9fdf3dba93a7778fd6ae4cc0951ccf401ffdbe
Eric Masi <eric.masi.dev@gmail.com>
2025-02-18 08:21:08 -0500

    Get test to pass

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
 func main() {
-	//
+	c := flag.Bool("c", false, "Return the number of bytes in a file")
+	flag.Parse()
+
+	if flag.NArg() == 0 {
+		fmt.Println("missing path to file")
+		os.Exit(1)
+	}
+
+	p := os.Args[len(os.Args)-1]
+
+	if *c {
+		info, err := os.Stat(p)
+		if err != nil {
+			fmt.Printf("cannot get file info, %s", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%d %s", info.Size(), p)
+	}
 }
