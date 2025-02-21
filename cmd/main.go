30635227d2febe6b14ee5da9ff713aaea3402ee4
Eric Masi <eric.masi.dev@gmail.com>
2025-02-21 16:24:24 -0500

    Extract function

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,16 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
 
 func main() {
-	countBytes := flag.Bool("c", false, "Return the number of bytes in a file")
-	countLines := flag.Bool("l", false, "Return the number of lines in a file")
-	countWords := flag.Bool("w", false, "Return the number of words in a file")
+	wantBytes := flag.Bool("c", false, "Return the number of bytes in a file")
+	wantLines := flag.Bool("l", false, "Return the number of lines in a file")
+	wantWords := flag.Bool("w", false, "Return the number of words in a file")
+	wantCharacters := flag.Bool("m", false, "Return the number of characters in a file")
 	flag.Parse()
 
 	if flag.NArg() == 0 {
@@ -21,7 +23,7 @@ func main() {
 
 	pathToFile := os.Args[len(os.Args)-1]
 
-	if *countBytes {
+	if *wantBytes {
 		info, err := os.Stat(pathToFile)
 		if err != nil {
 			fmt.Printf("cannot get file info, %s", err)
@@ -31,7 +33,7 @@ func main() {
 		fmt.Printf("%d %s\n", info.Size(), pathToFile)
 	}
 
-	if *countLines {
+	if *wantLines {
 		file, err := os.Open(pathToFile)
 		if err != nil {
 			fmt.Printf("cannot open file, %s", err)
@@ -49,7 +51,7 @@ func main() {
 		fmt.Printf("%d %s\n", count, pathToFile)
 	}
 
-	if *countWords {
+	if *wantWords {
 		file, err := os.Open(pathToFile)
 		if err != nil {
 			fmt.Printf("cannot open file, %s", err)
@@ -70,4 +72,44 @@ func main() {
 
 		fmt.Printf("%d %s\n", count, pathToFile)
 	}
+
+	if *wantCharacters {
+		file, err := os.Open(pathToFile)
+		if err != nil {
+			fmt.Printf("cannot open file, %s", err)
+			os.Exit(1)
+		}
+		defer file.Close()
+
+		count, err := countCharacters(file)
+		if err != nil {
+			fmt.Printf("cannot count characters, %s", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%d %s\n", count, pathToFile)
+	}
+}
+
+func countCharacters(file *os.File) (int, error) {
+	scanner := bufio.NewScanner(file)
+	count := 0
+
+	for scanner.Scan() {
+		r := bufio.NewReader(strings.NewReader(scanner.Text()))
+		for {
+			_, _, err := r.ReadRune()
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+
+				return -1, err
+			}
+
+			count++
+		}
+	}
+
+	return count, nil
 }
